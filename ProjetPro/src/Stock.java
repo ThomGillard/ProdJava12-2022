0a1f3df385e104332d3d5ab420173e02885e8e61
MatJgs <119605120+MatJgs@users.noreply.github.com>
2022-12-02 14:30:57 +0100

    Add files via upload

--- ProjetPro/src/Stock.java
+++ ProjetPro/src/Stock.java
@@ -3,21 +3,24 @@ public class Stock  {
     private Adresse adresse;
     private int tailleMax;
 
-    public int getTailleMax() {
-        return tailleMax;
-    }
+    int[] tabQuantite = new int[tailleMax];
+    Produit[] tabProduit = new Produit [tailleMax];
 
-    public void setTailleMax(int tailleMax) {
-        this.tailleMax = tailleMax;
-    }
+    int nbProduits=0;
 
+
+
+    //Constructeur
     public Stock(Adresse adresse, int tailleMax) {
         this.adresse = adresse;
         this.tailleMax = tailleMax;
     }
-
-    int[] tabQuantite = new int[tailleMax];
-    Produit[] tabProduit = new Produit [tailleMax];
-
+    public int getTailleMax() {
+        return tailleMax;
+    }
+    public void ajouterProduit(Produit produit){
+        tabProduit[nbProduits]=produit;
+        nbProduits++;
+    }
 
 }
