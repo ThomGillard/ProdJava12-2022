605883f66b4f2870c928e497f365f06798118deb
MatJgs <119605120+MatJgs@users.noreply.github.com>
2022-12-02 14:24:09 +0100

    Add files via upload

--- ProjetPro/src/Réfrigéré.java
+++ ProjetPro/src/Réfrigéré.java
@@ -1,11 +1,11 @@
 class Réfrigéré extends Produit{
 
-    public Réfrigéré(String nom, double prix, String marque, boolean estToxique) {
+
+    double temperature = 0;
+    public Réfrigéré(String nom, double prix, String marque, boolean estToxique,double temperature) {
         super(nom, prix, marque, estToxique);
         this.temperature = temperature;
     }
-    double temperature = 0;
-
     public double getTemperature() {
         return temperature;
     }
