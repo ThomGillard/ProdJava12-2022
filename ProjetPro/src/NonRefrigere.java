39b4c94cf366002eef4484be37c39acc0fe330a4
Thomas Gillard <119607894+ThomGillard@users.noreply.github.com>
2022-12-02 12:27:49 +0100

    Delete NonRefrigere.java

--- ProjetPro/src/NonRefrigere.java
+++
@@ -1,8 +0,0 @@
-
-
-public class NonRefrigéré extends Produit{
-
-    public NonRefrigéré(String nom, int prix, String marque, boolean estToxique) {
-        super(nom, prix, marque, estToxique);
-    }
-}
