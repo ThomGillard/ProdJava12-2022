594fd9dd58670ec0b6da7edc6bb4eb591d06901c
Thomas Gillard <thomas.gillard@hotmail.com>
2022-12-02 11:22:21 +0100

    Initial commit

---
+++ ProjetPro/src/Adresse.java
@@ -0,0 +1,43 @@
+public class Adresse {
+    private String rue;
+    private int numero;
+    private String nomVille;
+    private int codePostal;
+
+
+
+    public String getRue() {
+        return rue;
+    }
+
+    private void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    private void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNomVille() {
+        return nomVille;
+    }
+
+    private void setNomVille(String nomVille) {
+        this.nomVille = nomVille;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    private void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+
+
+}
---
+++ ProjetPro/src/Stock.java
@@ -0,0 +1,15 @@
+public class Stock  {
+
+    private Adresse adresse;
+
+    public int getTailleMax() {
+        return tailleMax;
+    }
+
+    public void setTailleMax(int tailleMax) {
+        this.tailleMax = tailleMax;
+    }
+
+    private int tailleMax;
+
+}
