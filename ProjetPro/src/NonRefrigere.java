df78031cbc82da2c931475876e328e77a7eeaa9f
Glayus <Francoisdebois@gmail.com>
2022-12-02 11:49:00 +0100

    Add files via upload

---
+++ ProjetPro/src/NonRefrigere.java
@@ -0,0 +1,8 @@
+
+
+public class nonRefrigere extends Produit{
+
+    public nonRefrigere(String nom, int prix, String marque, boolean estToxique) {
+        super(nom, prix, marque, estToxique);
+    }
+}
