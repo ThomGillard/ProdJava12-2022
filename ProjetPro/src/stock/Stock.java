3f37f5efac3aa9de926d4d7b3eac23628c081dae
Thomas Gillard <thomas.gillard@hotmail.com>
2022-12-03 19:33:22 +0100

    V3

--- ProjetPro/src/stock/Stock.java
+++ ProjetPro/src/stock/Stock.java
@@ -4,27 +4,17 @@ import produit.Produit;
 
 public class Stock {
 
-    private Adresse adresse;
+    private String adresse;
     private int tailleMax;
-
-    int[] tabQuantite = new int[tailleMax];
-    Produit[] tabProduit = new Produit[tailleMax];
-
-    int nbProduits=0;
-
 
 
     //Constructeur
-    public Stock(Adresse adresse, int tailleMax) {
+    public Stock(String adresse, int tailleMax) {
         this.adresse = adresse;
         this.tailleMax = tailleMax;
     }
     public int getTailleMax() {
         return tailleMax;
-    }
-    public void ajouterProduit(Produit produit){
-        tabProduit[nbProduits]=produit;
-        nbProduits++;
     }
 
 }
