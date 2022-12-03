b894758bfbc58a214838a9cf84636c647dee3fc1
Thomas Gillard <thomas.gillard@hotmail.com>
2022-12-03 16:18:06 +0100

    3

--- ProjetPro/src/produit/ProduitGestion.java
+++ ProjetPro/src/produit/ProduitGestion.java
@@ -1,19 +1,14 @@
 package produit;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ProduitGestion {
+    private final static Scanner sc= new Scanner(System.in);
 
     private final ArrayList<Produit> produits = new ArrayList();
     private static final int MAX_TEMPERATURE_FRIGO = 8;
 
-        /* public void nouveauProduit (String nom,double prix,String marque, boolean estToxique){
-                Produit nouveauProduit = new nouveauProduit();
-                produits.add(nouveauProduit);
-
-        } */
-
-    //
 
     // Produit normal   --> nom, prix, marque, toxique
     // Produit refrigéré --> nom prix marque toxique, temperature
@@ -21,7 +16,13 @@ public class ProduitGestion {
 
     public void creerProduit(String nom, double prix, String marque, boolean estToxique, int temperature) throws CreerProduitException{
 
-        // Nom non vide
+        // Nom non vide:
+            try {
+                return Integer.parseInt(sc.nextLine());
+            }catch (NumberFormatException e){
+                return -1;
+            }
+        }
         // Prix positif
         // Marque non vide
         // temperature entre ...
