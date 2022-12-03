3690618614aac83b37705f9f2a98b0577fdf397c
MatJgs <119605120+MatJgs@users.noreply.github.com>
2022-12-03 17:12:05 +0100

    Add files via upload

--- ProjetPro/src/stock/StockGestion.java
+++ ProjetPro/src/stock/StockGestion.java
@@ -1,4 +1,27 @@
 package stock;
 
+import produit.CreerProduitException;
+
+import java.util.ArrayList;
+
 public class StockGestion {
+
+    private final ArrayList<Stock> stocks = new ArrayList<>();
+
+    public void créerStock(String adresse, int tailleMax) throws CreerStockException {
+
+        // adresse non vide
+        if (adresse == null|| adresse.length()==0){
+            throw new CreerStockException("L'adresse ne peut être nulle");
+        }
+        // Taille positive
+        if (tailleMax<=0){
+            throw new CreerStockException("La taille ne peut être nulle ou négative");
+        }
+        
+        Stock nouveauStock = new Stock(adresse,tailleMax);
+        this.stocks.add(nouveauStock);
+
+
+    }
 }
