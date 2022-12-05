3fcfac0df61dfb07776091b762771d13bbff9a7b
MatJgs <119605120+MatJgs@users.noreply.github.com>
2022-12-05 09:18:47 +0100

    Add files via upload

--- ProjetPro/src/Main.java
+++ ProjetPro/src/Main.java
@@ -1,5 +1,7 @@
 import produit.*;
+import stock.CreerStockException;
 import stock.Stock;
+import stock.StockGestion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +18,11 @@ public class Main {
             "6. Quitter";
     private final static Scanner sc= new Scanner(System.in);
     private final static ProduitGestion produitGestion = new ProduitGestion();
+    private final static StockGestion stockGestion = new StockGestion();
 
     public static void main(String[] args) {
 
         int choixMenu;
-
 
 
         do {
@@ -32,16 +34,19 @@ public class Main {
                 switch (choixMenu){
                     case 1 : afficherNouveauProduit();
                         break;
-//            case 2: nouveauStock;
-//            break,
-//            case 3 : produitEnStock;
-//            break;
-//            case 4 : modifQuantStock;
-//            break;
-//            case 5 : suppProduit; Mathis
+                    case 2: afficherNouveauStock();
+                        break;
+                        //ajouter produit à un stock
+                    case 3 : ajouterProduitAStock();
+                        break;
+                    case 4 : modifierQuantite();
+                        break;
+//            case 5 : suppProduit;
 //            break;
                 }
-            } else {
+            } else if (choixMenu==6){
+                System.out.println("Vous quittez le programme.");
+            }else{
                 afficherMessageErreurMenu();
             }
 
@@ -53,13 +58,14 @@ public class Main {
     }
 
     private static void afficherMessageErreurMenu() {
-        System.out.println("Veuillez entrer un choix entre 1 et 6");
+        System.out.println("Veuillez entrer un choix entre 1 et 5");
     }
 
+    //CASE 1
     private static void afficherNouveauProduit(){
 
         System.out.println("Nom du produit :");
-            String nom = sc.next();
+        String nom = sc.next();
 
         System.out.println("Prix du produit :");
         double prix= sc.nextDouble();
@@ -69,17 +75,21 @@ public class Main {
         String marque = sc.next();
 
         // vérification pour produit toxique en userfriendly
-            int choixToxique;
-            do {
+        int choixToxique;
+        do {
+
             System.out.println("Taper 1 si le produit est toxique et 2 si non");
             choixToxique=sc.nextInt();
-        }while (choixToxique <1 || choixToxique >2);
-            boolean estToxique = choixToxique == 1;
-        // température ;
-            double temperature;
-                System.out.println("Quelle est la température de conservation du produits ? Comprise entre 0° et 30°");
-                temperature= sc.nextDouble();
+            sc.nextLine();
 
+        }while (choixToxique <1 || choixToxique >2);
+
+        boolean estToxique = choixToxique == 1;
+        // température
+        double temperature;
+        System.out.println("Quelle est la température de conservation du produits ? Comprise entre 0° et 30°");
+        temperature= sc.nextDouble();
+        sc.nextLine();
 
         System.out.println("Produit créé avec succès");
 
@@ -93,6 +103,92 @@ public class Main {
         System.out.println("Nb produits " + produitGestion.getNbProduits());
     }
 
+    //CASE 2
+    private static void afficherNouveauStock(){
+        System.out.println("Adresse : ");
+        String adresse = sc.nextLine();
+
+        System.out.println("Taille max du stock :");
+        int tailleMax = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Nom du stock :");
+        String nomStock = sc.next();
+        sc.nextLine();
+
+        int choixToxique;
+        do {
+
+            System.out.println("Taper 1 si le produit est toxique et 2 si non");
+            choixToxique=sc.nextInt();
+            sc.nextLine();
+
+        }while (choixToxique <1 || choixToxique >2);
+
+        boolean estToxique = choixToxique == 1;
+
+        int choixFrais;
+        do {
+
+            System.out.println("Taper 1 si le produit est frais et 2 si non");
+            choixFrais=sc.nextInt();
+            sc.nextLine();
+
+        }while (choixFrais <1 || choixFrais >2);
+
+        boolean estFrais = choixFrais == 1;
+
+
+
+        System.out.println("Stock créé avec succès");
+
+        try {
+            stockGestion.creerStock(adresse,tailleMax,nomStock,estToxique,estFrais);
+        } catch (CreerStockException e) {
+            System.out.println("Erreur dans l'encodage. Le message d'erreur est le suivant:");
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    //CASE 3
+    private static void ajouterProduitAStock(){
+        System.out.println("Nom du produit à ajouter : ");
+        String nomProduit = sc.next();
+
+        System.out.println("Nom du stock dans lequel ajouter :");
+        String nomStock = sc.next();
+
+
+        System.out.println("Quantité du produit :");
+        int quantite = sc.nextInt();
+        sc.nextLine();
+
+
+
+        stockGestion.ajouterProduitAStock(produitGestion,nomProduit,nomStock,quantite);
+
+        //Gérer pour pas que ça s'affiche quand plein
+        //System.out.println("Ajout effectué avec succès");
+        int qtt = stockGestion.getNbStockProduits();
+        System.out.println(qtt + " produit dans le stock");
+    }
+
+    //CASE 4
+    private static void modifierQuantite(){
+        System.out.println("Nom du produit à modifier : ");
+        String nomProduit = sc.next();
+
+        System.out.println("Quantité du produit :");
+        int quantite = sc.nextInt();
+        sc.nextLine();
+
+
+        stockGestion.modifierQuantite(nomProduit,quantite);
+        int qtt = stockGestion.getQuantite(nomProduit);
+        System.out.println("La quantité est de "+qtt+ " dans ce stock");
+    }
 
 
 
